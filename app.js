b4c765e17c33431cd4a63c05758b2ec1cbaec93a
eji6u94g <seanmybear@gmail.com>
2021-07-12 18:03:46 +0800

    feat: 1. import mongoose 2. set up mongoose and Schema setting

--- app.js
+++ app.js
@@ -1,12 +1,24 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
 const restaurantList = require('./restaurant.json')
+const mongoose = require('mongoose')
 const app = express()
 const port = 3000
+const dbConnection = mongoose.connection
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 app.use(express.static('public'))
+
+//set mongoose
+mongoose.connect('mongodb://localhost/restaurant_list', { useNewUrlParser: true, useUnifiedTopology: true })
+dbConnection.on('error', () => {
+  console.log('Mongodb error!')
+})
+dbConnection.once('open', () => {
+  console.log('Mongodb is connected!')
+})
+
 
 app.get('/', (req, res) => {
   res.render('index', { restaurants: restaurantList.results })
