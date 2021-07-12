7d6fb8d319fdd0768c3a0e4cb2e07630749e0107
eji6u94g <seanmybear@gmail.com>
2021-07-12 19:56:48 +0800

    feat: replace all json data with data gotten from mongoDB

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
-const restaurantList = require('./restaurant.json')
+// const restaurantList = require('./restaurant.json')
 const mongoose = require('mongoose')
 const restaurantData = require('./models/restaurant.js')
 const app = express()
@@ -20,20 +20,36 @@ dbConnection.once('open', () => {
   console.log('Mongodb is connected!')
 })
 
-
+//Index page
 app.get('/', (req, res) => {
-  res.render('index', { restaurants: restaurantList.results })
+  restaurantData.find()
+    .lean()
+    .then(restaurants => res.render('index', { restaurants }))
+    .catch(error => console.log(error))
 })
 
+//show page
 app.get('/restaurants/:restaurant_id', (req, res) => {
-  const restaurants = restaurantList.results.find(item => item.id.toString() === req.params.restaurant_id)
-  res.render('show', { restaurants })
+  const id = req.params.restaurant_id
+  restaurantData.findById(id)
+    .lean()
+    .then(restaurants => res.render('show', { restaurants }))
+    .catch(error => console.log(error))
 })
 
+function restaurantsMatchKeyword(restaurant, keyword) {
+  return restaurant.name.trim().toLowerCase().includes(keyword) || restaurant.category.trim().toLowerCase().includes(keyword)
+}
+
+//search
 app.get('/search', (req, res) => {
-  const keyword = req.query.keyword
-  const restaurants = restaurantList.results.filter(item => item.name.trim().toLowerCase().includes(keyword.trim().toLowerCase()) || item.category.trim().toLowerCase().includes(keyword.trim().toLowerCase()))
-  res.render('index', { restaurants, keyword })
+  const keyword = req.query.keyword.trim().toLowerCase()
+  restaurantData.find()
+    .lean()
+    .then(restaurants => {
+      restaurants = restaurants.filter(restaurant => restaurantsMatchKeyword(restaurant, keyword))
+      res.render('index', { restaurants, keyword: req.query.keyword })
+    })
 })
 
 app.listen(port, () => {
